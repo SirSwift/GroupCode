537a1407290c4a175589a36540a25aea930aec22
Kabir singh <agouraappclub@gmail.com>
2022-09-29 20:38:34 -0700

    Initial Commit

---
+++ groupProjectUITests/groupProjectUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  groupProjectUITestsLaunchTests.swift
+//  groupProjectUITests
+//
+//  Created by AgouraAppClub on 9/29/22.
+//
+
+import XCTest
+
+final class groupProjectUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
