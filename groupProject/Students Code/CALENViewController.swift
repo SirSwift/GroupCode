f266a6362fd76d265feddfd98d1be80263d6fe97
SirSwift <114188879+SirSwift@users.noreply.github.com>
2022-10-03 21:00:52 -0700

    Update CALENViewController.swift

--- groupProject/Students Code/CALENViewController.swift
+++ groupProject/Students Code/CALENViewController.swift
@@ -8,6 +8,46 @@
 import UIKit
 
 class CALENViewController: UIViewController {
+    
+    import UIKit
+
+class KabirViewController: UIViewController, UITableViewDelegate,UITableViewDataSource{
+
+    struct temp {
+        let label2:String
+    }
+    let data :[temp] = [
+        temp(label2: "hiiiii"),
+    ]
+
+
+    func tableView( tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+
+    func tableView( tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let home = data[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! CalenIntroTableViewCell
+        cell.label.text = home.label2
+        return cell
+    }
+@IBOutlet weak var tableview: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
+
+
+
+
+class CalenIntroTableViewCell: UITableViewCell {
+    @IBOutlet weak var label: UILabel!
+
+
+}
 
     override func viewDidLoad() {
         super.viewDidLoad()
