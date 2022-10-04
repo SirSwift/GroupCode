84240c759cdd6389078f3fd71f494fe3b7803ef4
SirSwift <114188879+SirSwift@users.noreply.github.com>
2022-10-03 19:26:09 -0700

    Update CALENViewController.swift

--- groupProject/CALENViewController.swift
+++ groupProject/CALENViewController.swift
@@ -27,3 +27,57 @@ class CALENViewController: UIViewController {
     */
 
 }
+
+class ViewController: UIViewController {
+    let button = UIButton(frame: CGRect(x: 10, y: 500, width: 360, height: 40))
+    let label = UILabel(frame: CGRect(x: 10, y: 450, width: 360, height: 40))
+    let mytextfield = UITextField(frame: CGRect(x: 10, y: 400, width: 360, height: 40))
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        Createbutton()
+        CreateLabel()
+        createtextfield()
+        CreateLabel()
+    }
+    func Createbutton(){
+        //creates a button
+        button.backgroundColor = UIColor.black
+        button.setTitle("Print", for: .normal)
+        button.layer.cornerRadius = 15.0
+        //adds action to button
+        button.addTarget(self, action: #selector(ButtonAction), for: .touchUpInside)
+        //making button visable
+        self.view.addSubview(button)
+    }
+    func CreateLabel(){
+        //creates a label
+        label.backgroundColor = UIColor.black
+        label.textColor = UIColor.white
+        label.textAlignment = NSTextAlignment.center
+        label.text = "Label"
+        label.layer.masksToBounds = true
+        label.layer.cornerRadius = 15.0
+
+        //making label visable
+        self.view.addSubview(label)
+    }
+    func createtextfield (){
+        mytextfield.backgroundColor = UIColor.green
+        mytextfield.textColor = UIColor.yellow
+        mytextfield.layer.cornerRadius = 9.0
+        mytextfield.setleftpadding(10)
+        self.view.addSubview(mytextfield)
+    }
+    @objc func ButtonAction (sender: UIButton){
+        //prints hi
+        label.text = mytextfield.text
+    }
+
+}
+extension UITextField {
+    func setleftpadding (_ amount:CGFloat){
+        let paddingview = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.size.height))
+        self.leftView = paddingview
+        self.leftViewMode = .always
+    }
+}
