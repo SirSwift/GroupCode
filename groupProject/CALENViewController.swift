f1479f3f24e152268d809c385c90d7804c876f40
SirSwift <114188879+SirSwift@users.noreply.github.com>
2022-10-11 21:00:48 -0700

    Update CALENViewController.swift

--- groupProject/CALENViewController.swift
+++ groupProject/CALENViewController.swift
@@ -27,3 +27,56 @@ class CALENViewController: UIViewController {
     */
 
 }
+
+
+
+
+
+
+
+
+
+
+
+//10/11/2022 CLASS CODE
+
+class CALENViewController: UIViewController {
+    
+    @IBOutlet weak var Loginpress:UIButton!
+    @IBOutlet weak var SignUP:UIButton!
+    @IBOutlet weak var User:UITextField!
+    @IBOutlet weak var Password:UITextField!
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func login(_ sender: Any) {
+        
+        Auth.auth().createUser(withEmail: email, password: password) { 
+            authResult, error in
+        }                                                                  
+    }
+    
+    @IBAction func signup(_ sender: Any) {
+        let email = User.text!
+        let password = Password.text!
+        
+        Auth.auth().createUser(withEmail:email, password: password) { 
+            authResult, error in
+        }
+    }
+}
+    
+    
+    
+    
+    
+    
+    
+    
+}
